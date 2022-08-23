d8533e79c55713317152f8cf53d44789f66ef644
Martin Sieiro <sieirogutierrezmartin@gmail.com>
2022-08-23 17:16:52 +0200

    💄 updated page title

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,9 +1,17 @@
 import "@styles/global.scss";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Dev Articles Visualizer</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
